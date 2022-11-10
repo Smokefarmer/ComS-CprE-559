6539d5ee7381b729b8c037147e1093426b79a8d7
Rauchi <rauchbauer.t@gmail.com>
2022-11-10 17:59:42 -0600

    chainge index page

---
+++ webproject/src/main/resources/webConnAPI.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+@SuppressWarnings("serial")
+public class webConnAPI extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(request.getParameter("name"));
+}

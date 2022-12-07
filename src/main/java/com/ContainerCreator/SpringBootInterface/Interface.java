d4832fa84cd40147b4400138d9be974e6696f139
Rauchi <rauchbauer.t@gmail.com>
2022-12-07 00:53:59 -0600

    debuging login

--- src/main/java/com/ContainerCreator/SpringBootInterface/Interface.java
+++ src/main/java/com/ContainerCreator/SpringBootInterface/Interface.java
@@ -77,10 +77,10 @@ public class Interface {
 		return "User not found";
 	}
 	
-	//@RequestBody String awsAccessKeyId,@RequestBody String awsSecretAccessKey,@RequestBody String region
 	@PostMapping(path="/login", consumes="application/json", produces="application/json")
 	public String EC2Creator(@RequestBody CCCredentials credentials) { //Region example: us-east-2
 		SHAHash Hash = new SHAHash();
+		System.out.printf(credentials.getAwsAccessKeyId(), credentials.getAwsSecretAccessKey(), credentials.getRegion());
 		String ID = Hash.generateSHAHash(credentials.getAwsAccessKeyId(), credentials.getAwsSecretAccessKey(), credentials.getRegion());
 		Client newUser = new Client(ID,credentials.getAwsAccessKeyId(), credentials.getAwsSecretAccessKey(), credentials.getRegion());
 
